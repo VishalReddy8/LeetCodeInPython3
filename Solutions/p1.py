d293d3510d50317a4debc8057fa61d2ff869c6f6
manish24ts <126053896+manish24ts@users.noreply.github.com>
2024-08-11 23:03:17 +0530

    Update p1.py

---
+++ Solutions/p1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i, num in enumerate(nums): 
+            if target - num in nums_dict:  
+                return [nums_dict[target - num], i]  
+            nums_dict[num] = i
+
+''' 
+enumeration of list num will give values 0,1,2,3.. to i and values of list to num 
+
+while adding to dictionary keys are the num values and values are the i values
+
+target = (target - num) + num
+
+return the index values of (target - num) and num
+'''
