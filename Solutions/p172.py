201b45ace428278c9550adfa0507d28ca1a958fe
manish24ts <126053896+manish24ts@users.noreply.github.com>
2024-08-15 20:49:18 +0530

    Create p172.py

---
+++ Solutions/p172.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0 
+        i = 5
+        while n // i >= 1:
+            count += n // i
+            i *= 5
+        return count
+
+'''
+Maths 
+'''
