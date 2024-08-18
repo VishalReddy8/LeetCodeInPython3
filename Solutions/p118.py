c3fb6cffd823b392ed93ce761bf85ab840c00c0f
manish24ts <126053896+manish24ts@users.noreply.github.com>
2024-08-18 21:19:14 +0530

    Create p118.py

---
+++ Solutions/p118.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for i in range(numRows):
+            row = [1]
+            if i > 0:
+                last_row = triangle[-1]
+                for j in range(1, i):
+                    row.append(last_row[j-1] + last_row[j])
+                row.append(1)
+            triangle.append(row)
+        return triangle
+
+'''
+Self explanatory
+'''
