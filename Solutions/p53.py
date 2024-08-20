2227ed8ac453e242c9a8a13d8bd6774fbf25203a
manish24ts <126053896+manish24ts@users.noreply.github.com>
2024-08-20 19:28:24 +0530

    Create p53.py

---
+++ Solutions/p53.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        total = 0
+
+        for n in nums
+            if total < 0:
+                total = 0
+
+            total += n
+            res = max(res, total)
+        
+        return res
+
+  '''
+  Idea is to traverse and do it in o(n) time
+  '''
