9bc42cbb8bfda4e4fd514001f8f4674559792cd1
T S Manish <126053896+manish24ts@users.noreply.github.com>
2025-04-14 23:28:08 +0530

    Create p32.py

---
+++ Solutions/p32.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        maxlen = 0 
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    maxlen = max(maxlen, i - stack[-1])
+                    
+
+        return maxlen
+        
